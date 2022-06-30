8dca52781294477d179d71b13d82d177c03b0702
temmytope88 <fakiletemitope@gmail.com>
2022-06-30 01:20:27 +0100

    python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,19 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-a = number % 10
-if a > 5:
-    print(f'Last digit of {number} is {a} and is greater than 5\n')
-elif a == 0:
-    print(f'Last digit of {number} is {a} and is 0\n')
-else:
-    print(f'Last digit of {number} is {a} and is less than 6 and not 0\n')
+if number < 0:
+    a = ((number * -1) % 10)
+    if a > 5:
+        print(f'Last digit of {number} is {a} and is greater than 5')
+    elif a == 0:
+        print(f'Last digit of {number} is {a} and is 0')
+    else:
+        print(f'Last digit of {number} is {a} and is less than 6 and not 0')
+elif number > 0 or number == 0:
+    a = number % 10
+    if a > 5:
+        print(f'Last digit of {number} is {a} and is greater than 5')
+    elif a == 0:
+        print(f'Last digit of {number} is {a} and is 0')
+    else:
+        print(f'Last digit of {number} is {a} and is less than 6 and not 0')
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -5,4 +5,4 @@ for x in range(0,10):
         if x == 9 and y == 9:
             print(f'{x}{y}\n')
         else:
-            print(f'{x}{y},')
+            print(f'{x}{y}, ', end = "")
