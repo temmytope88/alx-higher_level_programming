ae6a2ea601ffe30b11aee8e592f03a3a8effbb9b
temmytope88 <fakiletemitope@gmail.com>
2022-06-30 01:44:57 +0100

    python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+from turtle import ycor
+
+
 for x in range(0, 10):
     for y in range(0,10):
         if x == 9 and y == 9:
-            print(f'{x}{y}')
+            print("{}{}".format(x,y))
         else:
-            print(f'{x}{y}, ', end = "")
+            print("{}{}, ".format(x,y), end = "")
