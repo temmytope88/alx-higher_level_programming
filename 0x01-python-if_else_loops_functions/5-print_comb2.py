008385977a07fb892725d15c881fa14c6e61e48d
temmytope88 <fakiletemitope@gmail.com>
2022-06-30 02:19:37 +0100

    python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-from turtle import ycor
-
-
-for x in range(0, 10):
-    for y in range(0,10):
-        if x == 9 and y == 9:
-            print("{}{}".format(x,y))
-        else:
-            print("{}{}, ".format(x,y), end = "")
+for x in range(0, 100):
+    if x < 99:
+        print("{:0>2d}".format(x), end = ", ")
+    elif x == 99:
+        print("{}".format(x))
