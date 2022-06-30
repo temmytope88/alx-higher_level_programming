b88385b743d69a25e9e537939fd6fc83c7371003
temmytope88 <fakiletemitope@gmail.com>
2022-06-30 01:32:09 +0100

    python

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for x in range(0,99):
+for x in range(0, 99):
     print(f'{x} = {hex(x)}')
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for x in range(0,10):
+for x in range(0, 10):
     for y in range(0,10):
         if x == 9 and y == 9:
-            print(f'{x}{y}\n')
+            print(f'{x}{y}')
         else:
             print(f'{x}{y}, ', end = "")
