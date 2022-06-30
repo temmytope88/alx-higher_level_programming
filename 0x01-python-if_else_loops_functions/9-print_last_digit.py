4968905819ec2a72d8b0fea1c456041fb26b1ab9
temmytope88 <fakiletemitope@gmail.com>
2022-06-30 23:40:02 +0100

    python

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,8 +3,14 @@
 def print_last_digit(number):
   if number < 0:
     rem = ((-1 * number) % 10)
+    print(rem, end='')
     return rem
   elif number > 0:
     rem = number % 10
+    print(rem, end='')
     return rem
+  elif number == 0:
+    print(number, end='')
+    return number
+    
   
