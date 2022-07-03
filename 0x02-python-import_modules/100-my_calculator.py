05caa7a205b4114a160fbbb67000c63c84e68691
temmytope88 <fakiletemitope@gmail.com>
2022-07-03 17:08:55 +0100

    python modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+from unittest import result
+import calculator_1
+num = len(sys.argv)
+
+if num != 4:
+  print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+  exit(1)
+else:
+  a =  int(sys.argv[1])
+  b = int(sys.argv[3])
+  opr = sys.argv[2]
+  if opr not in ["+", "-", "/", "*" ]:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+  else:
+    if opr == "+":
+      result = calculator_1.add(a, b)
+      print("{} + {} = {}".format(a, b, result))
+    elif opr == "-":
+      result = calculator_1.sub(a, b)
+      print("{} - {} = {}".format(a, b, result))
+    elif opr == "/":
+      result = calculator_1.div(a, b)
+      print("{} / {} = {}".format(a, b, result))  
+    else:
+      result = calculator_1.mul(a, b)
+      print("{} * {} = {}".format(a, b, result))
