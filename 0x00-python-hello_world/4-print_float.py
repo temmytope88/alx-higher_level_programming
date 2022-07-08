b31380838476a92735286a2a301334fcd2b44005
temmytope88 <fakiletemitope@gmail.com>
2022-07-08 17:51:43 +0100

    python

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3 
-number = 3.14159 
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f'float: {round(number,2)}')
+#!/usr/bin/python3
+number = 3.14159
+print(f"Float: {number:{1}.{3}}")
