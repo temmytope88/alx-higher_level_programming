d9cd9f6886e0c073f2ade754fef409a795f6eb72
temmytope88 <fakiletemitope@gmail.com>
2022-07-11 17:30:01 +0100

    python import module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     length = len(sys.argv)
     num = length - 1
     if num == 0:
-        print("{} argumnets".format(num))
+        print("{} argumnets:".format(num))
     elif num == 1:
-        print("{} argument".format(num))
+        print("{} argument:".format(num))
         print("{}: {}".format(num, sys.argv[num]))
     else:
-        print("{} arguments".format(num))
+        print("{} arguments:".format(num))
         for x in range(1, length):
             print("{}: {}".format(x, sys.argv[x]))
