192f5a23d7048c043fd8840603395dbf19fe77da
temmytope88 <fakiletemitope@gmail.com>
2022-09-09 17:03:50 +0100

    'correction'

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- selct from a table score and name where score >=10
 -- order by score in descending order
-select score, name from second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- updating a row where the name is Bob with the score set to 10
-UPDATE second_table SET score = 10 WHERE name = Bob;
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
