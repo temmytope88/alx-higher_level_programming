d5845fdabf949cb71581428cdf1e6dc133a7c413
temmytope88 <fakiletemitope@gmail.com>
2022-09-09 17:32:45 +0100

    correction

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- a query to select city and determine their average temperatures
-SELECT city, AVG(value) AS avg_temp FROM temperature GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
