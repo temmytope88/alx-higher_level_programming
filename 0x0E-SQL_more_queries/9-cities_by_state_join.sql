80f498b7392f3420edd0489bf2fc26f23eea8175
temmytope88 <fakiletemitope@gmail.com>
2022-09-09 18:15:36 +0100

    more SQL queries

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- select from a multiple table using join
-SELECT cities.id, cities.name, state.name FROM cities INNER JOIN state ON cities.state_id = state.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, state.name FROM hbtn_0d_usa.cities INNER JOIN state ON cities.state_id = state.id ORDER BY cities.id ASC;
