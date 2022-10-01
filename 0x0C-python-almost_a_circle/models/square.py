f0440d64e227b1b089a7feb8189d01e244f1a835
temmytope88 <fakiletemitope@gmail.com>
2022-10-01 04:35:29 +0100

    update square method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,10 +20,63 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """return the size of a square object
+
+        Returns:
+            size: size a square object
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """sets the value of size
+        """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """it retruns [Square] (<id>) <x>/<y> - <width>/<height>
         """
         value = "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y,
-            self.width)
+            self.size)
         return value
+
+    def update(self, *args, **kwargs):
+        """ A function that update the attributes of
+            a rectangle object
+        """
+        if args:
+            if len(args) == 4:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+
+            if len(args) == 3:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+
+            if len(args) == 2:
+                self.id = args[0]
+                self.size = args[1]
+
+            if len(args) == 1:
+                self.id = args[0]
+
+        else:
+            for key, value in kwargs.items():
+                if key == "size":
+                    self.width = value
+
+                if key == "id":
+                    self.id = value
+
+                if key == "x":
+                    self.x = value
+
+                if key == "y":
+                    self.y = value
