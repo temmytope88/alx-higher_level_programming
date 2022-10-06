fc6b9291eb438c451a088c14ffb8a28b73ef5b2f
temmytope88 <fakiletemitope@gmail.com>
2022-10-06 01:45:48 +0100

    Js class

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
