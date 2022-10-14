c2ad4f7dafadf267a3687655e61f140755255d2b
temmytope88 <fakiletemitope@gmail.com>
2022-10-14 02:46:40 +0100

    filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3], port=3306, charset='utf8')
     cur = db.cursor()
-    value  = sys.argv[4]
+    value = sys.argv[4]
     cur.execute("""SELECT * FROM states
                 WHERE states.name = '{:s}'
                 ORDER BY id ASC""".format(value))
