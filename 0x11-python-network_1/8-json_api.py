f59dbee0b866209709521529ae5b9d6c61d71027
temmytope88 <fakiletemitope@gmail.com>
2022-10-25 18:28:44 +0100

    http response using requests module

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" A module the uses the requests
+module for http request """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if (sys.argv[2]):
+        q = sys.argv[2]
+    else:
+        q = ""
+
+    data = {"q": q}
+    r = requests.post(url, data)
+
+    if r.json():
+        data = r.json()
+        if data:
+            print("[<{}>] <{}>".format(data.id, data.nme))
+        else:
+            print("No result")
+    else:
+        print("Not a valid JSON")
