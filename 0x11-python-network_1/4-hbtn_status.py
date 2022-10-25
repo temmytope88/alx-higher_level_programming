a17e572c73ef5f78bb1b9e971faa5f722f0e915b
temmytope88 <fakiletemitope@gmail.com>
2022-10-25 10:05:43 +0100

    http request using request lib

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a module of http request
+using request package """
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    a = type(r)
+    b = type(r.text)
+    c = r.text
+    print('Body request:')
+    print('\t- type: {}'.format(b))
+    print('\t- content: {}'.format(c))
