8c04dea2af47422c2e17c0cfa2227f143be1ec95
temmytope88 <fakiletemitope@gmail.com>
2022-10-25 16:51:28 +0100

    http request using request package

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,9 +8,9 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
-    r.status_code
+    value = r.status_code
     if r.status_code == requests.codes.ok:
         c = r.text
         print(c)
     else:
-        print("Error code: {}".format(r.status_code()))
+        print("Error code: {:d}".format(value))
