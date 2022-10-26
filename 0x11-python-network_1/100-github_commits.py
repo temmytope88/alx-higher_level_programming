ca7954fe3d026d3dbc23f26dc4856ac275c863df
temmytope88 <fakiletemitope@gmail.com>
2022-10-26 08:46:21 +0100

    http request using request module

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" A module the uses the requests
+module for http request """
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".\
+        format(owner, repository)
+
+    r = requests.get(url)
+    info = r.json()
+
+    try:
+        for i in range(0, 10):
+            data = info[i]
+            sha = data.get('sha')
+            name = data.get('commit').get('author').get('name')
+            print("{}: {}".format(sha, name))
+    except KeyError as e:
+        print("None")
