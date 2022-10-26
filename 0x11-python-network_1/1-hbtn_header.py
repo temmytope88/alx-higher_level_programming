07d8ac1a20ef290860f82b41868c0caccee9bd19
temmytope88 <fakiletemitope@gmail.com>
2022-10-26 15:55:11 +0100

    http request using requests module

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.\
-         urlopen(url) as response:
+    with urllib.request.urlopen(url) as response:
         header = response.info()
         print(header['X-Request-Id'])
