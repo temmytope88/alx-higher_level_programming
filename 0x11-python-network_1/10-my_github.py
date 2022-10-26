7916606f1680e60e4f10bc9db20506a575714206
temmytope88 <fakiletemitope@gmail.com>
2022-10-26 04:43:05 +0100

    http request using requests modules

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     #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
     #sys.argv[2]
     #header = {"Authorization": "token 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"}
-    r = requests.get(url, auth=HTTPBasicAuth(username, token))
+    r = requests.get(url, auth=(HTTPBasicAuth(username, token)))
     info = r.json()
-    print(len(info))
-    print(info)
+    print(info['id'])
