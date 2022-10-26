229ab2cb37f94decc3e24e1d71c1101798f87391
temmytope88 <fakiletemitope@gmail.com>
2022-10-26 04:50:06 +0100

    http request using requests modules

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     url = "https://api.github.com/user"
     username = sys.argv[1]
     token = sys.argv[2]
-    #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
-    #sys.argv[2]
-    #header = {"Authorization": "token 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"}
     r = requests.get(url, auth=(HTTPBasicAuth(username, token)))
     info = r.json()
-    print(info['id'])
+
+    try:
+        print(info["id"])
+    except KeyError as e:
+        print("None")
